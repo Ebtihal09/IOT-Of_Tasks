c21959c243874108567a954b8d73598886992382
Ebtihal Salim <124944456+Ebtihal09@users.noreply.github.com>
2023-07-28 17:00:27 +0300

    Add files via upload

---
+++ Task-1/Read-PHP-File.ino
@@ -0,0 +1,66 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+const String url = "https://s-m.com.sa/smtc/test/hbvfd/retrieve.php";
+String payload = "";
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  pinMode(25, OUTPUT);
+  pinMode(26, OUTPUT);
+  pinMode(27, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("OK! IP=");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Fetching " + url + "... ");
+
+}
+
+void loop() {
+  HTTPClient http;
+  http.begin(url);
+  int httpResponseCode = http.GET();
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+    Serial.println();
+    Serial.println(payload);
+
+    if( payload == "right" ){
+      digitalWrite(25, HIGH);
+    }
+    if(payload == "left"){
+      digitalWrite(26, HIGH);
+    }
+    if(payload == "forward"){
+      digitalWrite(27, HIGH);
+    }
+    if(payload == "backward"){
+      digitalWrite(14, HIGH);
+    }
+    if(payload == "stop"){
+      digitalWrite(5, HIGH);
+    }
+  }
+
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+    Serial.println(":-(");
+  }
+  
+}
